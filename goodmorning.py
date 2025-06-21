304f4f5cd4a16933ca3bd5925c7be738d5609db0
Anjali-Bhawani <anjalibhawani1011@gmail.com>
2025-06-21 23:43:07 +0530

    time module

---
+++ goodmorning.py
@@ -0,0 +1,6 @@
+s = 19
+i = 1
+n = 10
+while i <= n :
+    print(19*i)
+    i = i + 1
---
+++ time_1.py
@@ -0,0 +1,7 @@
+import time
+s = 5
+while s > 0 :
+    print(s)
+    time.sleep(1)
+    s = s-1
+print("break time is over")
